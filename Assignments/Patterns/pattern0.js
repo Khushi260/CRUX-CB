4f466a48528940f6db2b75d6cac7f6446640c49a
Khushi Jha <94845508+Khushi260@users.noreply.github.com>
2022-09-30 09:48:37 +0530

    Create pattern0.js

---
+++ Assignments/Patterns/pattern0.js
@@ -0,0 +1,16 @@
+package lec3;
+import java.util.*;
+
+public class Pattern_0 {
+ public static void main(String[] args) {
+	 
+	 Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+	 int cst=0;
+	 
+    while(cst<=n) {
+    	System.out.print("*");
+    	cst++;
+    }
+}
+}
