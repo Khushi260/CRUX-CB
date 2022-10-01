4f3f1893598706bfc3104ef8c3fe480bd6723d8a
Khushi Jha <94845508+Khushi260@users.noreply.github.com>
2022-10-01 21:53:30 +0530

    Create Simple Input.java

---
+++ Assignments/Basics/Simple Input.java
@@ -0,0 +1,25 @@
+// https://hack.codingblocks.com/app/contests/3567/206/problem
+
+
+import java.util.*;
+public class Main {
+    public static void main(String args[]) {
+      
+      Scanner scn = new Scanner(System.in);
+      int sol=0;
+
+      while(sol>=0){
+
+          int num = scn.nextInt();
+          sol = sol+num;
+
+          if(sol>=0){
+              System.out.println(num);
+          }
+
+      }
+
+
+
+    }
+}
