89da81a3ea2a57ab68649bc97952a6d58a36b0a8
Khushi Jha <94845508+Khushi260@users.noreply.github.com>
2022-10-01 21:55:17 +0530

    Create Print reverse.java

---
+++ Assignments/Basics/Print reverse.java
@@ -0,0 +1,26 @@
+// https://hack.codingblocks.com/app/contests/3567/100/problem
+
+
+
+import java.util.*;
+public class Main {
+    public static void main(String args[]) {
+        
+        Scanner scn = new Scanner(System.in);
+		int num = scn.nextInt();
+
+        int digit;
+        int ans = 0;
+
+        while( num>0){
+              
+              digit = num%10;
+
+              ans = ans*10 + digit;
+              num=num/10;
+              
+        }
+
+        System.out.println(ans);
+    }
+}
