310a8fa6536b1eeb27e3aa35beb4d83556e78867
Khushi Jha <94845508+Khushi260@users.noreply.github.com>
2022-11-18 20:47:35 +0530

    Create Matrix Search.java

---
+++ Assignments/2D Arrays/Matrix Search.java
@@ -0,0 +1,74 @@
+/*
+Given an n x m matrix, where every row and column is sorted in increasing order, and a number x . Find if element x is present in the matrix or not.
+
+Input Format
+First line consists of two space separated integers N and M, denoting the number of element in a row and column respectively. 
+Second line of each test case consists of N*M space separated integers denoting the elements in the matrix in row major order. 
+Third line of each test case contains a single integer x, the element to be searched.
+
+Constraints
+1 <= N,M <= 30 0 <= A[i] <= 100
+
+Output Format
+Print 1 if the element is present in the matrix, else 0.
+
+Sample Input
+3 3
+3 30 38
+44 52 54
+57 60 69
+62
+Sample Output
+0
+Explanation
+Search the element in the sorted matrix. If the element is present print 1 otherwise print 0. In the sample input,in first case 62 is not present 
+in the matrix so 0 is printed. Similarly, for second case 55 is present in the matrix so 1 is printed.
+*/
+
+
+
+
+
+
+
+
+import java.util.*;
+public class Main {
+    public static void main(String args[]) {
+        Scanner scn = new Scanner(System.in);
+        int row = scn.nextInt();
+        int col = scn.nextInt();
+        int[][] arr = new int[row][col];
+
+     for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+           arr[i][j]=scn.nextInt();
+        }
+     }
+
+     int target = scn.nextInt();
+    
+     int ans = search(arr,row,col,target);
+     System.out.println(ans);
+    }
+
+    public static int search(int[][] arr,int row,int col,int target){
+
+        int i=0, j=col-1;
+     while(i<row && j>=0){
+
+         if(arr[i][j]==target){
+             return 1;
+         }
+         else if(arr[i][j]>target){
+             j--;
+         }
+         else{
+             i++;
+         }
+
+        }
+
+       return 0; 
+    }
+}
